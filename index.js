363f7a693a54375eb13409bf67a01825ced9ab30
Efrem Ropelato <efrem.ropelato@gmail.com>
2019-07-19 14:57:09 +0200

    add TRUNCATE statement

--- index.js
+++ index.js
@@ -10,4 +10,5 @@ pgClient.connect();
 pgClient.query('LISTEN "myEvent"');
 pgClient.on('notification', function (data) {
     console.log("\033[34m" + new Date + '-\033[0m payload', data.payload);
+    console.log("complete message=> ",data);
 });
